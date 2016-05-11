10c2b68366d8036a0ae63166acf12ad5563729f3
Galy <piguanghua365@163.com>
2016-05-11 15:27:35 +0800

    增加TbItemParam 这个Dao。MapBatis对于Text字段的数据使用XXXWithBolgs来查询，否则只能查基本数据。

---
+++ Shop_Dao/src/main/java/com/it/taotao/dao/TbItemParamDao.java
@@ -0,0 +1,13 @@
+package com.it.taotao.dao;
+
+import com.it.taotao.pojo.TbItemParam;
+
+import java.util.List;
+
+/**
+ * Created by 55 on 2016/5/11.
+ */
+public interface TbItemParamDao {
+    public TbItemParam getTbItemDescByItemCatId(Long itemCatId);
+    public List<TbItemParam> getItemParamList();
+}
---
+++ Shop_Dao/src/main/java/com/it/taotao/dao/impl/TbItemParamDaoImpl.java
@@ -0,0 +1,42 @@
+package com.it.taotao.dao.impl;
+
+import com.it.taotao.dao.TbItemParamDao;
+import com.it.taotao.dao.mapper.TbItemParamMapper;
+import com.it.taotao.pojo.TbItemDesc;
+import com.it.taotao.pojo.TbItemParam;
+import com.it.taotao.pojo.TbItemParamExample;
+import com.it.train.util.CollectionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by 55 on 2016/5/11.
+ */
+@Repository
+public class TbItemParamDaoImpl implements TbItemParamDao {
+    @Autowired
+    TbItemParamMapper tbItemParamMapper;
+
+
+    @Override
+    public TbItemParam getTbItemDescByItemCatId(Long itemCatId) {
+        TbItemParamExample tbItemParamExample = new TbItemParamExample();
+        TbItemParamExample.Criteria criteria =tbItemParamExample.createCriteria();
+        criteria.andItemCatIdEqualTo(itemCatId);
+        List<TbItemParam> listTbTtemParam = tbItemParamMapper.selectByExampleWithBLOBs(tbItemParamExample);
+        if(CollectionUtil.isEmpty(listTbTtemParam)){
+            return null;
+        }else{
+            return listTbTtemParam.get(0);
+        }
+    }
+
+    @Override
+    public List<TbItemParam> getItemParamList() {
+        TbItemParamExample tbItemParamExample = new TbItemParamExample();
+        return tbItemParamMapper.selectByExampleWithBLOBs(tbItemParamExample);
+    }
+}
